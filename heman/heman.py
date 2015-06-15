1448032e362827c0d5f64a3365b80594958e5bf6
Eduard Carreras <ecarreras@gisce.net>
2015-06-15 18:45:53 +0200

    First commit

---
+++ heman/heman.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from amoniak.utils import setup_empowering_api
+from amoniak.amon import make_uuid
+
+
+class HeMan(object):
+    def __init__(self, app=None):
+        """Initializized a HeMan object.
+        """
+        self.app = None
+        self.empowering = None
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        self.app = app
+        self.set_defaults()
+        self.empowering = setup_empowering_api(**self.app.config)
+
+    def set_defaults(self):
+        self.app.config.setdefault('HEMAN_PREFIX', '/heman')
