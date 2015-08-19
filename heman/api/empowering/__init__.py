7022bc63f4edc04e1cd1f0bb45620835534347cb
Eduard Carreras <ecarreras@gisce.net>
2015-08-19 09:47:27 +0200

    Add PeridRange for contract

--- heman/api/empowering/__init__.py
+++ heman/api/empowering/__init__.py
@@ -11,10 +11,12 @@ from heman.api.empowering.ot101 import OT101
 from heman.api.empowering.ot103 import OT103
 from heman.api.empowering.ot201 import OT201
 from heman.api.empowering.ot401 import OT401
+from heman.api.empowering.custom import PeriodRange
 
 resources = [
     (OT101, '/OT101Results/<contract>/<period>'),
     (OT103, '/OT103Results/<contract>/<period>'),
     (OT201, '/OT201Results/<contract>/<period>'),
-    (OT401, '/OT401Results/<contract>/<period>')
+    (OT401, '/OT401Results/<contract>/<period>'),
+    (PeriodRange, '/PeriodRange/<contract>')
 ]
