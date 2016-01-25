32bf7fe47d9f3be165f03c20b5d8060367800ab6
Eduard Carreras <ecarreras@gmail.com>
2016-01-25 15:49:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'r') as desc:
 
 setup(
     name='heman',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     url='https://github.com/gisce/heman',
     license='MIT',
