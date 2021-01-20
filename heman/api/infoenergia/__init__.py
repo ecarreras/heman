1ca26b380b12464a892b9aa8865d8e8e5474f214
Benjami <benjami94@gmail.com>
2021-01-20 08:44:58 +0100

    Add DistributionByTypeOfUse, DailyProfile, WeeklyProfile and MonthsProfile endpoints

--- heman/api/infoenergia/__init__.py
+++ heman/api/infoenergia/__init__.py
@@ -75,8 +75,93 @@ class DistributionByPeriod(InfoenergiaResource):
 
         return Response({}, mimetype='application/json')
 
+
+class DistributionByTypeOfUse(InfoenergiaResource):
+
+    def get(self, contract):
+        current_app.logger.debug(
+            'DistributionByTypeOfUse from Infoenergia Report, contract {}'.format(contract)
+        )
+
+        infoenergia_report = self.get_last_infoenergia_report(contract_name=contract)
+
+        have_results = infoenergia_report and \
+            infoenergia_report.get('results', {}).get('distributionByTypeOfUse')
+        if have_results:
+            return Response(
+                json.dumps(infoenergia_report['results']['distributionByTypeOfUse']),
+                mimetype='application/json'
+            )
+
+        return Response({}, mimetype='application/json')
+
+
+class DailyProfile(InfoenergiaResource):
+
+    def get(self, contract):
+        current_app.logger.debug(
+            'DailyProfile from Infoenergia Report, contract {}'.format(contract)
+        )
+
+        infoenergia_report = self.get_last_infoenergia_report(contract_name=contract)
+
+        have_results = infoenergia_report and \
+            infoenergia_report.get('results', {}).get('dailyTypicalProfileLast12Months')
+        if have_results:
+            return Response(
+                json.dumps(infoenergia_report['results']['dailyTypicalProfileLast12Months']),
+                mimetype='application/json'
+            )
+
+        return Response({}, mimetype='application/json')
+
+
+class WeeklyProfile(InfoenergiaResource):
+
+    def get(self, contract):
+        current_app.logger.debug(
+            'WeeklyProfile from Infoenergia Report, contract {}'.format(contract)
+        )
+
+        infoenergia_report = self.get_last_infoenergia_report(contract_name=contract)
+
+        have_results = infoenergia_report and \
+            infoenergia_report.get('results', {}).get('weeklyAvgConsumeLast3Months')
+        if have_results:
+            return Response(
+                json.dumps(infoenergia_report['results']['weeklyAvgConsumeLast3Months']),
+                mimetype='application/json'
+            )
+
+        return Response({}, mimetype='application/json')
+
+
+class MonthsProfile(InfoenergiaResource):
+
+    def get(self, contract):
+        current_app.logger.debug(
+            'MonthsProfile from Infoenergia Report, contract {}'.format(contract)
+        )
+
+        infoenergia_report = self.get_last_infoenergia_report(contract_name=contract)
+
+        have_results = infoenergia_report and \
+            infoenergia_report.get('results', {}).get('last3MonthsProfile')
+        if have_results:
+            return Response(
+                json.dumps(infoenergia_report['results']['last3MonthsProfile']),
+                mimetype='application/json'
+            )
+
+        return Response({}, mimetype='application/json')
+
+
 resources = [
     (InfoenergiaReport, '/InfoenergiaReport/<contract>'),
     (SeasonalProfile, '/InfoenergiaReport/data/seasonalprofile/<contract>'),
     (DistributionByPeriod, '/InfoenergiaReport/data/distributionbyperiod/<contract>'),
+    (DistributionByTypeOfUse, '/InfoenergiaReport/data/distributionbytypeofuse/<contract>'),
+    (DailyProfile, '/InfoenergiaReport/data/dailyprofile/<contract>'),
+    (WeeklyProfile, '/InfoenergiaReport/data/weeklyprofile/<contract>'),
+    (MonthsProfile, '/InfoenergiaReport/data/monthsprofile/<contract>'),
 ]
