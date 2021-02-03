7d7fbd3cc81fde70a6076b9157917a455c7d2035
Marta <marta.fernandez@somenergia.coop>
2021-02-03 14:59:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'r') as desc:
 
 setup(
     name='heman',
-    version='0.4.0',
+    version='1.1.1',
     packages=find_packages(),
     url='https://github.com/gisce/heman',
     license='MIT',
