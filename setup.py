6a9317696f3902c1cb3af4fc8d14a5d495c04113
Marta <marta.fernandez@somenergia.coop>
2021-02-12 11:22:27 +0100

    Bump to v1.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'r') as desc:
 
 setup(
     name='heman',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     url='https://github.com/gisce/heman',
     license='MIT',
