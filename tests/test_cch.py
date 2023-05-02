6b631ee4e6b808e71d29522cc3b08dcf7868d896
javikalsan <1070397+javikalsan@users.noreply.github.com>
2023-05-02 12:28:45 +0200

    ✅ Add initial snapshot for tg_cchfact use case

---
+++ tests/test_cch.py
@@ -0,0 +1,34 @@
+from yamlns import ns
+
+from testdata.curves import tg_cchfact_existing_points
+
+from heman.app import application
+
+
+class TestCchRequest(object):
+
+    def test_tg_cchfact_existing_points(self, yaml_snapshot):
+        token = tg_cchfact_existing_points['token']
+        cups = tg_cchfact_existing_points['cups']
+        date = tg_cchfact_existing_points['date']
+        endpoint_url = '/api/CCHFact/{cups}/{date}'.format(
+            cups=cups,
+            date=date
+        )
+        client = application.test_client()
+        headers = dict(
+            Authorization='token {token}'.format(token=token)
+        )
+
+        response = client.get(
+            endpoint_url,
+            headers=headers
+        )
+
+        assert response.status_code == 200
+
+        yaml_snapshot(ns(
+            status=response.status,
+            json=response.json,
+        ))
+
