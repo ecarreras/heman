825c82c9f199b5a95851eef7187ff9d2bea6ab13
javikalsan <1070397+javikalsan@users.noreply.github.com>
2023-05-03 11:35:10 +0200

    🎨 Improve readability

--- heman/api/cch/__init__.py
+++ heman/api/cch/__init__.py
@@ -13,11 +13,16 @@ from heman.config import mongo
 
 class CCHResource(AuthorizedResource):
     method_decorators = (
-        AuthorizedResource.method_decorators + [check_cups_allowed]
+            AuthorizedResource.method_decorators + [check_cups_allowed]
     )
 
     def options(self, *args, **kwargs):
         return jsonify({})
+
+
+WATT = 'W'
+KILOWATT = 'kW'
+P1_CURVE_TYPE = 'p'
 
 
 class CCHFact(CCHResource):
@@ -32,7 +37,7 @@ class CCHFact(CCHResource):
     def _curve_value(self, curve, unit):
         return {
             'date': time.mktime((curve['datetime']).timetuple()) * 1000,
-            'value': curve['ai'] * 1000 if unit == 'kW' else curve['ai']
+            'value': curve['ai'] * 1000 if unit == KILOWATT else curve['ai']
         }
 
     def ordered_merge(self, cursor_f1, cursor_p1):
@@ -41,21 +46,21 @@ class CCHFact(CCHResource):
         p1_curve = next(cursor_p1, False)
         while f1_curve and p1_curve:
             if f1_curve['datetime'] == p1_curve['datetime']:
-                curves.append(self._curve_value(f1_curve, 'kW'))
+                curves.append(self._curve_value(f1_curve, KILOWATT))
                 f1_curve = next(cursor_f1, False)
                 p1_curve = next(cursor_p1, False)
             elif f1_curve['datetime'] < p1_curve['datetime']:
-                curves.append(self._curve_value(f1_curve, 'kW'))
+                curves.append(self._curve_value(f1_curve, KILOWATT))
                 f1_curve = next(cursor_f1, False)
             else:
-                curves.append(self._curve_value(p1_curve, 'kW'))
+                curves.append(self._curve_value(p1_curve, KILOWATT))
                 p1_curve = next(cursor_p1, False)
 
         while f1_curve:
-            curves.append(self._curve_value(f1_curve, 'kW'))
+            curves.append(self._curve_value(f1_curve, KILOWATT))
             f1_curve = next(cursor_f1, False)
         while p1_curve:
-            curves.append(self._curve_value(p1_curve, 'kW'))
+            curves.append(self._curve_value(p1_curve, KILOWATT))
             p1_curve = next(cursor_p1, False)
 
         return curves
@@ -79,7 +84,7 @@ class CCHFact(CCHResource):
         p1_search_query = {
             'name': {'$regex': '^{}'.format(cups[:20])},
             'datetime': {'$gte': start, '$lt': end},
-            'type': 'p'
+            'type': P1_CURVE_TYPE
         }
         cursor_f5d = self.get_cursor_db(collection='tg_cchfact', query=search_query)
         cursor_f1 = self.get_cursor_db(collection='tg_f1', query=search_query)
@@ -87,8 +92,7 @@ class CCHFact(CCHResource):
 
         result = []
         if self._query_result_length(cursor_f5d) > 0:
-            for curve in cursor_f5d:
-                result.append(self._curve_value(curve, 'W'))
+            result = [self._curve_value(curve, WATT) for curve in cursor_f5d]
         elif self._query_result_length(cursor_f1) > 0 \
                 or self._query_result_length(cursor_p1) > 0:
             result = self.ordered_merge(cursor_f1, cursor_p1)
