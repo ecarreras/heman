389b0ae23100778018c9bcd4b6ac622de2152e44
javikalsan <1070397+javikalsan@users.noreply.github.com>
2023-05-03 13:00:11 +0200

    ✨🚧 Add mongo curve backend

---
+++ heman/api/cch/mongo_curve_backend.py
@@ -0,0 +1,34 @@
+from heman.config import mongo
+from pymongo import ASCENDING
+
+
+class MongoCurveBackend:
+
+    def get_cursor_db(self, collection, query):
+        return mongo.db[collection].find(
+            query,
+            fields={'_id': False, 'datetime': True, 'ai': True}).sort(
+            'datetime', ASCENDING
+        )
+
+    def build_query(
+        self,
+        start=None,
+        end=None,
+        cups=None,
+        **extra_filter
+    ):
+
+        query = {
+            'name': {'$regex': '^{}'.format(cups[:20])},
+            'datetime': {'$gte': start, '$lt': end}
+        }
+
+        return query
+
+    def get_curve(self, curve_type, start, end, cups=None):
+        query = self.build_query(start, end, cups, **curve_type.extra_filter)
+
+        result = self.get_cursor_db(curve_type.model, query)
+
+        return result
